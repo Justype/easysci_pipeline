e3b5ed8c1c2575bfe535e4402203cb37fb8c5f49
Justype <justype.net@outlook.com>
2025-09-14 21:17:30 -0400

    update the read_count_matrix function

--- workflow/utils/read_count_mtx.R
+++ workflow/utils/read_count_mtx.R
@@ -17,7 +17,7 @@ output_file <- ifelse(length(args) > 1, args[2], file.path(dirname(input_folder)
 message("Output file: ", output_file)
 gzip <- ifelse(length(args) > 2, args[3] != "gzip", TRUE)
 
-read_count_matrix <- function(folder, is_gzipped = TRUE) {
+read_count_matrix <- function(folder, is_gzipped = TRUE, symbol = TRUE) {
   require(Seurat); require(readr); require(tidyr); require(dplyr)
   cell_path <- ifelse(is_gzipped, "cell_annotation.csv.gz", "cell_annotation.csv")
   cell_path <- file.path(folder, cell_path)
@@ -25,53 +25,42 @@ read_count_matrix <- function(folder, is_gzipped = TRUE) {
   feature_path <- file.path(folder, feature_path)
   mtx_path <- ifelse(is_gzipped, "expression_matrix.mtx.gz", "expression_matrix.mtx")
   mtx_path <- file.path(folder, mtx_path)
-  
-  cells <- read_csv(cell_path, show_col_types = FALSE)
-  
+
+  gene_features <- read_csv(feature_path, show_col_types = FALSE)
+  is_human <- str_detect(gene_features$gene_id[1], "^ENSG")
+  is_mouse <- str_detect(gene_features$gene_id[1], "^ENSMUSG")
+  if (!is_human && !is_mouse) {
+    stop("The gene IDs in the feature annotation file must be either human (ENSG) or mouse (ENSMUSG).")
+  }
+
   mtx <- ReadMtx(
     mtx = mtx_path, features = feature_path, cells = cell_path,
-    feature.sep = ",", feature.column = 1, skip.feature = 1, # 1 ENSEMBL ID; 3 GENE SYMBOL
+    feature.sep = ",", feature.column = ifelse(symbol, 3, 1), skip.feature = 1, # 1 ENSEMBL ID; 3 GENE SYMBOL
     cell.sep = ",", cell.column = 1, skip.cell = 1
   )
-  
+
   so <- CreateSeuratObject(counts = mtx)
-  
+  cells <- read_csv(cell_path, show_col_types = FALSE)
   # Add metadata from the cell annotation file
   so@meta.data %>%
     rownames_to_column("cell_id") %>%
     left_join(cells, by = join_by("cell_id")) %>%
     column_to_rownames("cell_id") -> so@meta.data
-  
+
+  mt_pattern <- ifelse(is_human, "^MT-", "^mt-")
+    if (symbol) {
+    so[["percent.mt"]] <- PercentageFeatureSet(so, pattern = mt_pattern)
+  } else {
+    mt_gene_ids <- gene_features %>%
+      filter(str_detect(gene_name, mt_pattern)) %>%
+      pull(gene_id)
+    so[["percent.mt"]] <- PercentageFeatureSet(so, features = mt_gene_ids)
+  }
+
   return(so)
 }
 
-feature_annotation <- read_csv(
-  file.path(input_folder, ifelse(gzip, "feature_annotation.csv.gz", "feature_annotation.csv")),
-  show_col_types = FALSE
-)
-
-is_human <- str_detect(feature_annotation$gene_id[1], "^ENSG")
-is_mouse <- str_detect(feature_annotation$gene_id[1], "^ENSMUSG")
-
-if (!is_human && !is_mouse) {
-  stop("The gene IDs in the feature annotation file must be either human (ENSG) or mouse (ENSMUSG).")
-} else if (is_human) {
-  message("Detected human gene IDs (ENSG).")
-} else if (is_mouse) {
-  message("Detected mouse gene IDs (ENSMUSG).")
-}
-
-mt_gene_ids <- feature_annotation %>%
-  filter(str_detect(gene_name, ifelse(is_human, "^MT-", "^mt-"))) %>%
-  pull(gene_id)
-
-message("[", date(), "] Reading count matrix from folder: ", input_folder)
-
-so <- read_count_matrix(input_folder, gzip)
-
-message("[", date(), "] Read successfully. Ncells: ", ncol(so), ", Nfeatures: ", nrow(so))
-
-so[["percent.mt"]] <- PercentageFeatureSet(so, features = mt_gene_ids)
+so <- read_count_matrix(input_folder, is_gzipped = gzip, symbol = TRUE)
 
 # extension qs or rds
 if (str_detect(output_file, regex("\\.qs$", ignore_case = TRUE))) {
